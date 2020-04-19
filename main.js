63c4f6025fe2ad47a8cf2f27c409cc2ce71c9b4b
sadfsdf <mamogaaa@yandex.ru>
2020-04-19 12:40:06 +0300

    fixes

--- main.js
+++ main.js
@@ -8,33 +8,5 @@ const mb = menubar({
     webPreferences: {
       webviewTag: true 
     }
-    
   }
 })
-
-mb.on('ready', () => {
-  console.log('app is ready')
-  // your app code here
-})
-
-// app.on('ready', () => {
-//   // Register a 'CommandOrControl+X' shortcut listener.
-//   const ret = globalShortcut.register('MediaPlayPause', () => {
-//     console.log('MediaPlayPause is pressed')
-//   })
-
-//   if (!ret) {
-//     console.log('registration failed')
-//   }
-
-//   // Check whether a shortcut is registered.
-//   console.log(globalShortcut.isRegistered('MediaPlayPause'))
-// })
-
-// app.on('will-quit', () => {
-//   // Unregister a shortcut.
-//   globalShortcut.unregister('MediaPlayPause')
-
-//   // Unregister all shortcuts.
-//   globalShortcut.unregisterAll()
-// })
