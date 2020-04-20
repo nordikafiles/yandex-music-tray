93f403c4c0fd5d840ab7514197e62848c8382d17
sadfsdf <mamogaaa@yandex.ru>
2020-04-20 13:36:32 +0300

    fixes

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const { app, globalShortcut } = require('electron')
 const { menubar } = require('menubar')
+const path = require('path')
 
 const mb = menubar({
+  icon: path.join(__dirname, 'assets/menubar.png'),
   browserWindow: {
     width: 1000,
     height: 700,
