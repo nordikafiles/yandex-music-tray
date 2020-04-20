b82bfbb8f83193fcc76b2185e7848cc2bfcfb32b
sadfsdf <mamogaaa@yandex.ru>
2020-04-20 14:21:22 +0300

    added hotkeys

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, globalShortcut } = require('electron')
+const { app, globalShortcut, ipcMain } = require('electron')
 const { menubar } = require('menubar')
 const path = require('path')
 
@@ -8,7 +8,21 @@ const mb = menubar({
     width: 1000,
     height: 700,
     webPreferences: {
-      webviewTag: true 
+      webviewTag: true,
+      nodeIntegration: true
     }
   }
+})
+
+const KEYS = ['MediaPreviousTrack', 'MediaNextTrack', 'MediaPlayPause', 'Cmd+Shift+Option+4', 'Cmd+Shift+Option+5', 'Cmd+Shift+Option+6', 'Cmd+Shift+Option+7']
+
+app.on('ready', () => {
+  KEYS.map(x => globalShortcut.register(x, () => {
+    if (x == 'Cmd+Shift+Option+4') return mb.window.openDevTools()
+    mb.window.webContents.send('hotkey', x)
+  }))
+})
+
+app.on('will-quit', () => {
+  globalShortcut.unregisterAll()
 })
